990a8b4df9864df108fc772729854df3e4ee7071
Amir Mahmoud <amirebrahimi5@live.com>
2018-10-06 07:23:18 +0330

    Deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routs/api/Items');
 
@@ -14,11 +15,21 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose.connect(db)
-    .then( () => console.log('MongoDB connected'))
+    .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 // User Routs
 app.use('/api/items', items);
+
+// Serve Static assets if in production
+if (process.env.NODE_ENV == 'production') {
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const port = process.env.PORT || 5000;
 
